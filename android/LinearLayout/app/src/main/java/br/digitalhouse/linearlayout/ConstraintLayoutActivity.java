7d4cb74d110e635cdae62c7351682ad29a99f41e
Jéssica Milena Corrêa dos Santos <jcorrea@dhbrasil.digitalhouse.com>
2020-01-24 22:13:53 -0300

    projeto de layouts

---
+++ android/LinearLayout/app/src/main/java/br/digitalhouse/linearlayout/ConstraintLayoutActivity.java
@@ -0,0 +1,14 @@
+package br.digitalhouse.linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
