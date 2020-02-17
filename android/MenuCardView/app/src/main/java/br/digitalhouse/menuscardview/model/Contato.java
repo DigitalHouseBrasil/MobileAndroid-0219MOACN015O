9e71a99e1f3c55adf282c532e95f21f282372c49
jeehmilena <jessicamilenacorrea@gmail.com>
2020-02-17 19:24:57 -0300

    Prjeto da aula de recyclerView classe modelo

---
+++ android/MenuCardView/app/src/main/java/br/digitalhouse/menuscardview/model/Contato.java
@@ -0,0 +1,28 @@
+package br.digitalhouse.menuscardview.model;
+
+public class Contato {
+
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
