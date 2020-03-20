363ac78a4a0df375bb619c5641b312a54c7286d8
jeehmilena <jessicamilenacorrea@gmail.com>
2020-03-20 20:52:46 -0300

    projeto revisao

---
+++ android/RevisaoRoomRetrofit/app/src/main/java/com/example/albunsmusicasapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.albunsmusicasapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.albunsmusicasapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
